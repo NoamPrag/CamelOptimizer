bd8c4c2a5fae611f1f523123ac46e2d9f1084dff
Noam Prag <noam.prag0@gmail.com>
2023-04-02 00:34:04 +0300

    Add "binary search"-like optimization for range values

--- lib/camel_optimizer.ml
+++ lib/camel_optimizer.ml
@@ -1,1 +1,19 @@
 module Calculator = Calculator
+open Calculator
+
+let rec optimize (change_fn : person -> int -> person) (p : person)
+    (min_value : int) (max_value : int) =
+  let range = max_value - min_value in
+  let quarter_range = float_of_int range /. 4. |> ceil |> int_of_float in
+  let quarter_value = min_value + quarter_range in
+  let three_quarter_value = max_value - quarter_range in
+  if three_quarter_value = quarter_value then quarter_value
+  else
+    let quarter_res = calculate @@ change_fn p quarter_value in
+    let three_quarter_res = calculate @@ change_fn p three_quarter_value in
+    if quarter_res > three_quarter_res then
+      optimize change_fn p min_value three_quarter_value
+    else optimize change_fn p quarter_value max_value
+
+let optimize_height = optimize with_height
+let optimize_age = optimize with_age
